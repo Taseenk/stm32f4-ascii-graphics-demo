5984ed2497932694c26cc66f4395845c689a0c54
Taseen <taseenk@hotmail.com>
2025-12-20 02:26:16 +0100

    Define boolean values in main.h

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -31,6 +31,8 @@ extern "C" {
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#define TRUE    1   // Boolean True
+#define FALSE   0   // Boolean False
 
 /* USER CODE END Includes */
 
