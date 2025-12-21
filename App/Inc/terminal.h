1bf464e71b803c1b643cc2068d3b1d9d24b31353
Taseen <taseenk@hotmail.com>
2025-12-21 23:02:33 +0100

    add `ConsolePrintNewLine` using a single UART transmission

--- App/Inc/terminal.h
+++ App/Inc/terminal.h
@@ -12,13 +12,16 @@
 #include "main.h"
 
 /* Defines -------------------------------------------------------------------*/
-
+#define UART_BUFFER_SIZE    128      	// Size of the buffer for UART Rx/Tx (in bytes)
+#define TERMINAL_EOL        "\r\n"    // End of line characters for the terminal
 
 /* Externs -------------------------------------------------------------------*/
 
 
 /* Function prototypes -------------------------------------------------------*/
 uint8_t ConsolePrint(const char *str);
+uint8_t ConsolePrintNewLine(const char *str);
 uint8_t ConsolePrintDMA(const char *str);
+
 
 #endif /* __TERMINAL_H */
--- App/Src/terminal.c
+++ App/Src/terminal.c
@@ -13,6 +13,7 @@
 
 // Standard libraries
 #include <string.h>
+#include <stdio.h>
 
 /* Defines -------------------------------------------------------------------*/
 
@@ -44,7 +45,42 @@ uint8_t ConsolePrint(const char *str)
 
     // Check if the transmission was successful
     if (status != HAL_OK) {
-        // Return transmission failed
+        return FALSE; 
+    }
+
+    // Return Transmission successful
+    return TRUE;
+}
+
+/**
+ * @fn uint8_t ConsolePrintNewLine(const char *str)
+ * @brief Prints a null-terminated string followed by a newline using the private UART handle
+ * @param str The string to transmit
+ * @return TRUE if transmission was successful, FALSE otherwise.
+*/
+uint8_t ConsolePrintNewLine(const char *str)
+{
+    // Ensure both the UART handle and the data pointer are not NULL
+	if (p_uart->Instance == NULL || str == NULL)
+		return FALSE;
+
+    // Create a temporary buffer to hold the string with newline
+    char buffer[UART_BUFFER_SIZE];
+
+    // Append newline characters to the string
+    uint8_t len = snprintf(buffer, sizeof(buffer), "%s" TERMINAL_EOL, str);
+
+    // Check for snprintf errors or buffer overflow
+    if (len <= 0 || len >= sizeof(buffer))
+        return FALSE;
+
+    // Call the HAL UART transmit function to send the string
+	// HAL_MAX_DELAY ensures the function BLOCKS until all data is transmitted
+	// Will return the status of the HAL UART transmit
+    HAL_StatusTypeDef status = HAL_UART_Transmit(p_uart, (uint8_t *)buffer, len, HAL_MAX_DELAY);
+
+    // Check if the transmission was successful
+    if (status != HAL_OK) {
         return FALSE; 
     }
 
