d66d579e147e8a0640b5df9b6b09cf8cac20cd99
Taseen <taseenk@hotmail.com>
2026-01-18 21:47:16 +0100

    Add `MatrixCharacterNoise` function to simulate "Matrix" like digital rain effect

--- App/Inc/gfx_matrix_stream.h
+++ App/Inc/gfx_matrix_stream.h
@@ -13,9 +13,14 @@
 #include "main.h"
 
 /* Defines -------------------------------------------------------------------*/
+#define ASCII_PRINTABLE_START 33   // The '!' character
+#define ASCII_PRINTABLE_RANGE 94   // Total printable characters up to '~'
+#define COORDINATE_OFFSET     1    // Terminal coordinates usually start at 1,1
+#define STRING_TERMINATOR   '\0'    // Null terminator for strings
 
 /* typedefs ------------------------------------------------------------------*/
 
 /* Function prototypes -------------------------------------------------------*/
+void MatrixCharacterNoise(uint32_t frame, uint8_t density_scale);
 
 #endif /* __GFX_MATRIX_STREAM_H */
