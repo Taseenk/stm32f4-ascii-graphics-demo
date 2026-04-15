2c22651cbea972a29b64621c6efcde8b66b6c307
Taseen <taseenk@hotmail.com>
2026-04-15 03:19:51 +0200

    feat(terminal): add function prototype for `TerminalIsBufferReady`

--- App/Inc/terminal.h
+++ App/Inc/terminal.h
@@ -149,6 +149,7 @@ void TerminalPrint(const char *str);
 void TerminalPrintString(const char *str, uint16_t col, uint16_t row);
 
 // Framebuffer management
+uint8_t TerminalIsBufferReady(void);
 void TerminalBufferFlush(void);
 void TerminalBufferClear(void);
 void TerminalBufferDrawChar(char c, uint16_t col, uint16_t row);
