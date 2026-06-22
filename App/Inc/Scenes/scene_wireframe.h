3a294c5a9a4332925777b33c0e1c60ab26302625
Taseen <taseenk@hotmail.com>
2026-06-22 22:19:54 +0200

    feat(scene): add wireframe scene boilerplate with header and source files

---
+++ App/Inc/Scenes/scene_wireframe.h
@@ -0,0 +1,18 @@
+/**
+ ******************************************************************************
+ * @file           : scene_wireframe.h
+ * @brief          :
+ ******************************************************************************
+ */
+
+#ifndef __SCENE_WIREFRAME_H
+#define __SCENE_WIREFRAME_H
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Function prototypes -------------------------------------------------------*/
+void WireframeInit(void);
+void WireframeRender(uint32_t scene_frame);
+
+#endif /* __SCENE_WIREFRAME_H */
---
+++ App/Src/Scenes/scene_wireframe.c
@@ -0,0 +1,42 @@
+/**
+ ******************************************************************************
+ * @file           : scene_wireframe.c
+ * @brief          :
+ ******************************************************************************
+ */
+
+/* Includes ------------------------------------------------------------------*/
+// Project libraries
+#include "scene_wireframe.h"
+#include "terminal.h"
+
+// STM32 libraries
+#include "main.h"
+
+/* Private Defines -----------------------------------------------------------*/
+
+/* Private Typedefs ----------------------------------------------------------*/
+
+/* Private Variables ---------------------------------------------------------*/
+
+/* Private Function Prototypes -----------------------------------------------*/
+
+/* Private Functions ---------------------------------------------------------*/
+
+/* Public Functions ----------------------------------------------------------*/
+/**
+ * @fn void WireframeInit(void)
+ * @brief
+ */
+void WireframeInit(void)
+{
+}
+
+/**
+ * @fn void WireframeRender(uint32_t scene_frame)
+ * @brief
+ * @param scene_frame The current frame index provided by the scene manager.
+ */
+void WireframeRender(uint32_t scene_frame)
+{
+}
